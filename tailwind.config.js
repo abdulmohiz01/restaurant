b75964ef93b3dfc3530567d845c3e2b5a60e13c8
Abdul Mohiz <fatizforever@gmail.com>
2024-02-16 21:30:05 +0500

    Initials

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,26 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: '#303030',
+        accent: '#ffc200',
       },
+      fontFamily: {
+        grandhotel: ['Grand Hotel', 'cursive'],
+        monsterrat: ['Monsterrat', 'sans-serif'],
+      },
+    },
+  },
+  container: {
+    padding: {
+      DEFAULT: '15px',
     },
   },
   plugins: [],
